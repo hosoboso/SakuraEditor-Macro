72670374696159e1da8205c8d76b3a44e3063de3
hosoboso <139880091+hosoboso@users.noreply.github.com>
2024-07-08 22:49:37 +0900

    Update p.js

--- p.js
+++ p.js
@@ -4,7 +4,8 @@
 var Tag = "p";
 
 // <div class=""></div>のようにグローバル属性も入れたいならここに入力
-// var attribute = " class=\"\"";
+// var str = "class";
+// var attribute = " " + str + "=\"\"";
 
 //グローバル属性なしの場合
 var attribute = "";
@@ -13,7 +14,7 @@ var attribute = "";
 // テキストが選択されていれば選択されたテキストをタグで挟む
 if(Editor.GetSelectedString(0).length == 0) {
 	var ClipboardText = Editor.GetClipboard(0);
-	var ClipboardTag = "<" + Tag + attribute + ">" + "<" + Tag + "/>";
+	var ClipboardTag = "<" + Tag + attribute + ">" + "</" + Tag + ">";
 	Editor.InsText(ClipboardTag);
 } else {
 	var Selecttext = Editor.GetSelectedString(0);
