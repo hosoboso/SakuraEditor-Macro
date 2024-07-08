0f650454599c5bdff69026b314368dcec413aeab
hosoboso <139880091+hosoboso@users.noreply.github.com>
2024-07-08 21:12:33 +0900

    update

--- p.js
+++ p.js
@@ -3,15 +3,21 @@
 // 入力したいタグ　他タグのマクロはここを変更して作ればOK
 var Tag = "p";
 
+// <div class=""></div>のようにグローバル属性も入れたいならここに入力
+// var attribute = " class=\"\"";
+
+//グローバル属性なしの場合
+var attribute = "";
+
 // テキストが未選択の場合はタグだけ入力
 // テキストが選択されていれば選択されたテキストをタグで挟む
 if(Editor.GetSelectedString(0).length == 0) {
 	var ClipboardText = Editor.GetClipboard(0);
-	var ClipboardTag = "<" + Tag + ">" + "<" + Tag + "/>";
+	var ClipboardTag = "<" + Tag + attribute + ">" + "<" + Tag + "/>";
 	Editor.InsText(ClipboardTag);
 } else {
 	var Selecttext = Editor.GetSelectedString(0);
-	var SelectTag = "<" + Tag + ">" + Selecttext + "</" + Tag + ">";
+	var SelectTag = "<" + Tag  + attribute + ">" + Selecttext + "</" + Tag + ">";
 	Editor.InsText(SelectTag);
 }
 
