2365e5615a52096d6079975084f14bdfc2bb54d3
hosoboso <139880091+hosoboso@users.noreply.github.com>
2024-07-14 13:16:23 +0900

    update

--- hexadecimal.js
+++ hexadecimal.js
@@ -1,14 +1,13 @@
-//数値文字参照（文字参照）置換
-//何の文字でも16進数の数値文字参照に変換
+// 数値文字参照（文字参照）置換
+// 選択した文字列なら何の文字でも16進数の数値文字参照に変換
 
 var result = "";
 var inputStrings = Editor.GetSelectedString(0);
 
-inputStringsLength = inputStrings.length;
-
 // charCodeAt : 0スタートで、*番目の文字のUnicode("12371"や"12398")を返す。
-// "&#x" ";"と連結して、数値文字参照を作成
-for (var i=0; i<inputStringsLength; i++) {
-	result = result + "&#x" + inputStrings.charCodeAt(i).toString(16) + ";";
+// "&#x" ";"と連結して、数値文字参照を作成。
+for (var i = 0; i < inputStrings.length; i++) {
+	result += "&#x" + inputStrings.charCodeAt(i).toString(16) + ";";
 }
+// 結果表示
 Editor.InsText(result);
